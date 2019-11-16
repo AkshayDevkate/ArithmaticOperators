2c89d62fd2fc7255cd35782d27d1e18c72f5721c
Akshay Dattatray Devkate <42045980+AkshayDevkate@users.noreply.github.com>
2019-11-16 23:19:20 +0530

    Update AdditionViewController.swift
    added substraction code

--- AdditionViewController.swift
+++ AdditionViewController.swift
@@ -10,13 +10,18 @@ import UIKit
 
 class AdditionViewController: UIViewController {
     
- 
+    // text fields to input data
     @IBOutlet weak var Textlfield1: UITextField!
     
     @IBOutlet weak var textField2: UITextField!
+ 
     
-    
+    //to display addition output
     @IBOutlet weak var label: UILabel!
+   
+    
+    //to display substraction output
+    @IBOutlet weak var substractionlabel: UILabel!
     
     
     
@@ -32,7 +37,7 @@ class AdditionViewController: UIViewController {
         
     }
     
-  
+    //Addition code when the addition button is tapped
     @IBAction func ButtonTapped(_ sender: Any) {
         
         let firstValue = Double(Textlfield1.text!)
@@ -41,18 +46,14 @@ class AdditionViewController: UIViewController {
         
         if firstValue != nil && secoundValue != nil{
             
-            let outputValue = Double(firstValue! + secoundValue!)
-            
-          
+            let outputValue = Double(firstValue! + secoundValue
             
             label.text! = "Anwer is \(outputValue)"
-            
-         
-            
-            
+        
             
         }else{
             
+                //check whether the user hasent entered the number to do arithmatic operations i,e check if the labels are empty
             label.text! = "Please enter numbers"
         }
         
@@ -61,6 +62,28 @@ class AdditionViewController: UIViewController {
         
         
         
+        
+    }
+
+
+        
+        //substraction code whetn the user clicks the substraction button
+ @IBAction func SubstracionTapped(_ sender: Any) {
+        
+        
+       let firstValue1 = Double(Textlfield1.text!)
+                    
+             let secoundValue1 = Double(textField2.text!)
+            
+            if firstValue1 != nil && secoundValue1 != nil{
+                
+                let outputValue1 = Double(firstValue1! - secoundValue1!)
+                
+
+                substractionlabel.text! = "Anwer is \(outputValue1)"
+                
+                
+                
         
     }
     
