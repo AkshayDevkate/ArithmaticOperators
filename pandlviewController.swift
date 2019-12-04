d8ed5f24dd8b92bec5ab6dd09225cee15547cbb7
Akshay Dattatray Devkate <42045980+AkshayDevkate@users.noreply.github.com>
2019-12-04 23:36:30 +0530

    Create pandlviewController.swift

---
+++ pandlviewController.swift
@@ -0,0 +1,47 @@
+//
+//  mathopViewController.swift
+//  CalculatorApp
+//
+//  Created by Akshay Devkate on 04/12/19.
+//  Copyright © 2019 Akshay Devkate. All rights reserved.
+//
+
+import UIKit
+
+class mathopViewController: UIViewController {
+
+    @IBOutlet weak var Firstvalue: UITextField!
+    
+    @IBOutlet weak var secondValue: UITextField!
+    
+    @IBOutlet weak var UILabelAns: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func calculateTapped(_ sender: Any) {
+  
+    let investment = Double(Firstvalue.text!)
+        
+        let returns = Double(secondValue.text!)
+        
+        if investment != nil && returns != nil{
+            
+            let outputvalue = Double((((investment! - returns!)/investment!))*100)
+            
+            UILabelAns.text! = "Ans is \(outputvalue)"
+            
+        
+            
+        }else{
+            UILabelAns!.text = "Please enter the values"
+        }
+        
+        
+    }
+
+}
